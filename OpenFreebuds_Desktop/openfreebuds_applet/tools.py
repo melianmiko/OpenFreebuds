75f2b0f2a2cbbe80e6ed42bbbcd6e2e83d6865ed
MelianMiko <melianmiko@gmail.com>
2022-02-21 17:48:13 +0700

    fix x2

--- OpenFreebuds_Desktop/openfreebuds_applet/tools.py
+++ OpenFreebuds_Desktop/openfreebuds_applet/tools.py
@@ -22,18 +22,21 @@ def items_hash_string(items):
 
 
 def is_running():
-    for a in psutil.process_iter():
-        # Check base name
-        if "openfreebuds" in a.name():
-            return True
+    our_pid = os.getpid()
 
-        # Check cmdline, eg 'python -m openfreebuds'
-        try:
-            for b in a.cmdline():
-                if "openfreebuds" in b:
-                    return True
-        except psutil.AccessDenied:
-            pass
+    for a in psutil.process_iter():
+        if a.pid != our_pid:
+            # Check base name
+            if "openfreebuds" in a.name():
+                return True
+
+            # Check cmdline, eg 'python -m openfreebuds'
+            try:
+                for b in a.cmdline():
+                    if "openfreebuds" in b:
+                        return True
+            except psutil.AccessDenied:
+                pass
 
     return False
 
