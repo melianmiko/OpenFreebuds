10205956cde9378919c8641af00c16ed1ec27cc2
MelianMiko <melianmiko@gmail.com>
2022-05-04 15:27:41 +0700

    Fix updater crash when language isn't available

--- OpenFreebuds_Desktop/src/mmk_updater/_lc.py
+++ OpenFreebuds_Desktop/src/mmk_updater/_lc.py
@@ -33,8 +33,10 @@ L18N = {
 
 def t(k):
     lang = locale.getdefaultlocale()[0]
-    if k in L18N[lang]:
-        return L18N[lang][k]
+    if lang in L18N:
+        if k in L18N[lang]:
+            return L18N[lang][k]
+
     if k in BASE_LOCALE:
         return BASE_LOCALE[k]
     return k
