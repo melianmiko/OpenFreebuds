531a2183b1f4399d070a7606143c8fef132a7ea1
MelianMiko <melianmiko@yandex.ru>
2023-05-09 13:43:34 +0700

    Migrate to mmk_updater 0.14, minor fixes, bump version 0.12.2

---
+++ make_release.py
@@ -0,0 +1,31 @@
+import json
+import os
+import shutil
+import subprocess
+import sys
+
+sys.path.append(os.path.join(os.path.dirname(__file__), "src"))
+
+import version_info
+
+
+def main():
+	print(f"Make OpenFreebuds {version_info.VERSION}")
+	if sys.platform == "win32":
+		make_win32()
+	else:
+		print("OS not supported")
+
+
+def make_win32():
+	if os.path.isdir("dist"):
+		shutil.rmtree("dist")
+
+	subprocess.Popen(["npm.cmd", "run", "build"]).wait()
+	subprocess.Popen([r"venv\Scripts\pyinstaller.exe", "openfreebuds.spec"]).wait()
+	subprocess.Popen([r"C:\Program Files (x86)\NSIS\Bin\makensis.exe", "openfreebuds.nsi"])
+
+	os.rename(r"dist\openfreebuds.install.exe", f"dist\\openfreebuds_{version_info.VERSION}_win32.exe")
+
+
+main()
