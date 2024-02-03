3106899bd5035dc573ec586ecf89d2875b5141a6
MelianMiko <support@mmk.pw>
2024-02-03 12:15:53 +0700

    Mark 5i as fully supported, minor fixes

--- src/openfreebuds/device/__init__.py
+++ src/openfreebuds/device/__init__.py
@@ -1,30 +1,29 @@
 from openfreebuds.device.generic.base import BaseDevice
 from openfreebuds.device.generic.virtual_device import VirtualDevice
 from openfreebuds.device.huawei.profiles import (FreeLaceProDevice, FreeBuds4iDevice, FreeBudsSEDevice,
-                                                 FreeBudsPro3Device, FreeBuds5iDevice)
+                                                 FreeBudsPro3Device, FreeBuds5iDevice, FreeBudsProDevice)
 
 PROFILES = {
     "HUAWEI FreeBuds SE": FreeBudsSEDevice,
     "HUAWEI FreeLace Pro": FreeLaceProDevice,
     "HUAWEI FreeBuds 4i": FreeBuds4iDevice,
     "HUAWEI FreeBuds 5i": FreeBuds5iDevice,
+    "HUAWEI FreeBuds Pro": FreeBudsProDevice,
     "HUAWEI FreeBuds Pro 3": FreeBudsPro3Device,
     # "Debug: Virtual device": VirtualDevice
 }
 
 # (implementation_level, profile)
 SUPPORTED_DEVICES = {
-    "HUAWEI FreeBuds SE": ("full", FreeBudsSEDevice),
-
     "HUAWEI FreeLace Pro": ("full", FreeLaceProDevice),
-
+    "HUAWEI FreeBuds SE": ("full", FreeBudsSEDevice),
     "HUAWEI FreeBuds 4i": ("full", FreeBuds4iDevice),
-    "HONOR Earbuds 2 Lite": ("full", FreeBuds4iDevice),
-    "HUAWEI FreeBuds Pro 2": ("partial", FreeBuds4iDevice),
-
-    "HUAWEI FreeBuds 5i": ("partial", FreeBuds5iDevice),
-
+    "HUAWEI FreeBuds 5i": ("full", FreeBuds5iDevice),
+    "HUAWEI FreeBuds Pro": ("partial", FreeBudsProDevice),
+    "HUAWEI FreeBuds Pro 2": ("partial", FreeBuds5iDevice),
     "HUAWEI FreeBuds Pro 3": ("partial", FreeBudsPro3Device),
+
+    "HONOR Earbuds 2 Lite": ("full", FreeBuds4iDevice),
 
     # "Debug: Virtual device": ("virtual", VirtualDevice),
 }
