f77283a07808836ef7c266cd7354e380d9a9ae00
MelianMiko <support@mmk.pw>
2024-11-17 13:56:12 +0700

    [Misc] Release preparations

--- scripts/sync_translations.sh
+++ scripts/sync_translations.sh
@@ -3,41 +3,13 @@ PATH=/usr/lib/qt6/bin:$PATH
 
 cd "$(dirname "$0")"/../openfreebuds_qt
 
-# Sync base ts-file
-echo "-- Base locale update"
-pylupdate6 --no-obsolete --ts ./assets/i18n/en.ts .
-
-# Sync new *.po-files
-for po_file in ./assets/i18n/*.po
-do
-  [ -f "$po_file" ] || break
-  locale=`basename ${po_file%.*}`
-  if ! [ -f ./assets/i18n/$locale.ts ]
-  then
-    echo "-- Import $locale from PO-file"
-    lconvert ./assets/i18n/$locale.po -o ./assets/i18n/$locale.ts
-  fi
-done
-
-# Update exiting translations
 for ts_file in ./assets/i18n/*.ts
 do
   [ -f "$ts_file" ] || break
-  locale=`basename ${ts_file%.*}`
-  if [ ./assets/i18n/$locale.po -nt ./assets/i18n/$locale.ts ]
-  then
-    echo "-- Update $locale from PO-file"
-    lconvert ./assets/i18n/$locale.po -o ./assets/i18n/$locale.ts
-  else
-    # Create *.po-file for accent
-    echo "-- Update $locale"
-    lconvert ./assets/i18n/$locale.ts -o ./assets/i18n/$locale.po
 
-    if [[ "$locale" == "en" ]]
-    then
-      python ../scripts/sync_translations_fill_msgstr.py ./assets/i18n/$locale.po
-    fi
-  fi
-
-  [[ "$locale" == "en" ]] || pylupdate6 --no-obsolete --ts $ts_file .
+  pylupdate6 \
+    --no-obsolete \
+    --exclude scripts \
+    --exclude debian \
+    --ts $ts_file .
 done
--- scripts/sync_translations_fill_msgstr.py
+++
@@ -1,9 +0,0 @@
-import sys
-import polib
-
-po = polib.pofile(sys.argv[1])
-for entry in po:
-    if entry.msgstr == "":
-        entry.msgstr = entry.msgid
-
-po.save(sys.argv[1])
