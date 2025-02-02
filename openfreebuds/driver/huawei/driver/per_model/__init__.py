70b636b8074e843bf0dc6500fea005e7964369c5
MelianMiko <support@mmk.pw>
2025-02-02 21:55:09 +0700

    [Fix] Attach SE 2 driver (#54)

--- openfreebuds/driver/huawei/driver/per_model/__init__.py
+++ openfreebuds/driver/huawei/driver/per_model/__init__.py
@@ -5,5 +5,7 @@ from .buds_pro import OfbDriverHuaweiPro
 from .buds_pro_2 import OfbDriverHuaweiPro2
 from .buds_pro_3 import OfbDriverHuaweiPro3
 from .buds_se import OfbDriverHuaweiSe
+from .buds_se_2 import OfbDriverHuaweiSe2
 from .lace_pro import OfbDriverHuaweiLacePro
 from .lace_pro_2 import OfbDriverHuaweiLacePro2
+from .buds_studio import OfbDriverHuaweiStudio
